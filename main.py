a51a2399eb5b78a1ab407774925c0d08bd5c254f
AJITH ROY <ar84805@eanesisd.net>
2020-09-28 13:49:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter a number: "))
+op = input("Enter an operator: ")
+num2 = float(input("Enter your second number: "))
+
+if op == "+":
+  print(num1 + num2)
+elif op == "-":
+  print(num1 - num2)
+elif op == "*":
+  print(num1 * num2)
+elif op == "/":
+  print(num1 / num2)
+elif op == "^":
+  print(num1 ** num2)
+elif op == "%":
+  print(num1 % num2)
+else:
+  print("Invalid Operator")
+
